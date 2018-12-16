23aea826b2237b4e8a27c09845d2017a8c5ea981
joelNguyenn1010 <nguyenngocanh590@gmail.com>
2018-12-16 15:44:39 +0700

    adding css for order

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import { connect } from 'react-redux'
+import 'Order.css'
 import * as order from './testOrder.json'
 class Order extends React.Component {
     state = {
@@ -9,43 +10,43 @@ class Order extends React.Component {
     }
     componentDidMount() {
         //THIS IS FOR TEST AND CREATE UI
-        console.log(order.default)
+        // console.log(order.default)
 
 
         //THIS IS FOR PRODUCTION
-        // const headers = {
-        //     'Content-Type': 'application/json',
-        //     'Authorization': `JWT ${this.props.user.token}`
-        // }
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `JWT ${this.props.user.token}`
+        }
 
-        // axios.get('http://127.0.0.1:8000/api/order/', { headers })
-        //     .then(res => {
-        //         if(res.data && res.data.length > 0) {
-        //             console.log(res.data)
-        //             this.setState({
-        //                 orders: res.data
-        //             })
-        //         } else {
-        //             this.setState({
-        //                 orders: [],
-        //                 mess: "We can't find any order"
-        //             })
-        //         }
+        axios.get('http://127.0.0.1:8000/api/order/', { headers })
+            .then(res => {
+                if(res.data && res.data.length > 0) {
+                    console.log(res.data)
+                    this.setState({
+                        orders: res.data
+                    })
+                } else {
+                    this.setState({
+                        orders: [],
+                        mess: "We can't find any order"
+                    })
+                }
            
-        //     })
-        //     .catch(error => {
-        //         if (error.response.status === 401 || error.response.status === 403) {
-        //             this.setState({
-        //                 orders: [],
-        //                 mess: "You need to login in order to do that"
-        //             })
-        //         } else {
-        //             this.setState({
-        //                 orders: [],
-        //                 mess: "Server error"
-        //             })
-        //         }
-        //     })
+            })
+            .catch(error => {
+                if (error.response.status === 401 || error.response.status === 403) {
+                    this.setState({
+                        orders: [],
+                        mess: "You need to login in order to do that"
+                    })
+                } else {
+                    this.setState({
+                        orders: [],
+                        mess: "Server error"
+                    })
+                }
+            })
     }
 
     render() {
