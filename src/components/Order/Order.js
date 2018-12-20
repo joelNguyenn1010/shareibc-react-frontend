bf649643e39e48ae9a5240ad97c64ee712355915
William Cao <williamcao.cmu@gmail.com>
2018-12-20 08:51:52 +0700

    UI order

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react'
 import axios from 'axios'
 import { connect } from 'react-redux'
@@ -61,20 +62,132 @@ class Order extends React.Component {
                 // }
             })
     }
+=======
+import React from "react";
+import axios from "axios";
+import { connect } from "react-redux";
+import { headerS } from "../../store/actions/user-action";
+import {
+  MDBCol,
+  MDBRow,
+  MDBContainer,
+  MDBCardBody,
+  MDBCard,
+  MDBCardTitle,
+  MDBCardText,
+  MDBListGroup,
+  MDBListGroupItem
+} from "mdbreact";
+import "./Order.css";
+import * as order from "./testOrder.json";
+class Order extends React.Component {
+  state = {
+    orders: [],
+    mess: ""
+  };
+  componentDidMount() {
+    //THIS IS FOR TEST AND CREATE UI
+    // console.log(order.default)
 
-    render() {
-        return (
-            <div>
-                <h1>Order</h1>
-                <h1>{this.state.mess}</h1>
-            </div>
-        )
-    }
-}
-const mapStateToProps = (state) => {
-    return {
-        user: state.userReducer
-    }
-}
+    //THIS IS FOR PRODUCTION
+    const headers = this.props.user.headers;
+    // 'Content-Type': 'application/json',
+    // 'Authorization': `Bearer facebook ${this.props.user.token}`
+>>>>>>> Stashed changes
 
-export default connect(mapStateToProps)(Order)
+    console.log(headers);
+    axios
+      .get("http://127.0.0.1:8000/api/order/", { headers })
+      .then(res => {
+        if (res.data && res.data.length > 0) {
+          console.log(res.data);
+          this.setState({
+            orders: res.data
+          });
+        } else {
+          this.setState({
+            orders: [],
+            mess: "We can't find any order"
+          });
+        }
+      })
+      .catch(error => {
+        // if (error.response.status === 401 || error.response.status === 403) {
+        //     this.setState({
+        //         orders: [],
+        //         mess: "You need to login in order to do that"
+        //     })
+        // } else {
+        //     this.setState({
+        //         orders: [],
+        //         mess: "Server error"
+        //     })
+        // }
+      });
+  }
+
+  render() {
+    return (
+      <MDBContainer fluid>
+        <MDBRow>
+          <MDBCol size="12" md="4">
+            <MDBListGroup style={{ width: "100%" }}>
+              <MDBListGroupItem>Cras justo odio</MDBListGroupItem>
+              <MDBListGroupItem>Dapibus ac facilisis in</MDBListGroupItem>
+              <MDBListGroupItem>Morbi leo risus</MDBListGroupItem>
+              <MDBListGroupItem>Porta ac consectetur ac</MDBListGroupItem>
+              <MDBListGroupItem>Vestibulum at eros</MDBListGroupItem>
+            </MDBListGroup>
+          </MDBCol>
+          <MDBCol size="12" md="8">
+            <MDBCard>
+              <MDBCardBody>
+                <MDBRow>
+                  <MDBCol md="4">
+                    <img
+                      style={{
+                        height: "150px"
+                      }}
+                      src="https://mdbootstrap.com/img/Others/documentation/1.jpg"
+                      className="img-fluid"
+                      alt=""
+                    />
+                  </MDBCol>
+                  <MDBCol md="8">
+                    <MDBRow>
+                      <MDBCol md="3">Name</MDBCol>
+                      <MDBCol md="8">name Data</MDBCol>
+                    </MDBRow>
+                    <MDBRow>
+                      <MDBCol md="3">Name</MDBCol>
+                      <MDBCol md="8">name Data</MDBCol>
+                    </MDBRow>{" "}
+                    <MDBRow>
+                      <MDBCol md="3">Name</MDBCol>
+                      <MDBCol md="8">name Data</MDBCol>
+                    </MDBRow>{" "}
+                    <MDBRow>
+                      <MDBCol md="3">Name</MDBCol>
+                      <MDBCol md="8">name Data</MDBCol>
+                    </MDBRow>{" "}
+                    <MDBRow>
+                      <MDBCol md="3">Name</MDBCol>
+                      <MDBCol md="8">name Data</MDBCol>
+                    </MDBRow>
+                  </MDBCol>
+                </MDBRow>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    );
+  }
+}
+const mapStateToProps = state => {
+  return {
+    user: state.userReducer
+  };
+};
+
+export default connect(mapStateToProps)(Order);
